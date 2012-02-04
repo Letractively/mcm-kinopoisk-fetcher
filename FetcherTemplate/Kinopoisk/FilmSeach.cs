6d961ba0c0e7528426c6c9bd4b58820bb2f5114d
Andrey Grebennikov <andrey@basdesign.ru>
2012-02-04 14:08:45 +0400

    fix FilmSearch exceptions

--- FetcherTemplate/Kinopoisk/FilmSeach.cs
+++ FetcherTemplate/Kinopoisk/FilmSeach.cs
@@ -10,46 +10,98 @@ namespace FetcherTemplate.Kinopoisk
 {
     class FilmSeach:Abstract
     {
-        public List<MovieResult> Find(string title, string year)
+
+        protected readonly string FilmTitle;
+        protected readonly string FilmYear;
+
+        public FilmSeach(string title, string year = null)
+        {
+            FilmTitle = title;
+            FilmYear = year;
+        }
+
+
+        
+        protected string PageAddress
+        {
+            get
+            {
+                var title = Utils.FixTitleForSearching(FilmTitle); // fix wrnog symbols
+                var pattern = new Regex(@"\s+");
+                title = string.Join("+", pattern.Split(title));
+                if (!string.IsNullOrEmpty(FilmYear)) title += "+" + FilmYear;
+                return "http://www.kinopoisk.ru/level/7/type/film/list/1/find/" + Uri.EscapeUriString(title);    
+            }
+        }
+
+        private HtmlAgilityPack.HtmlDocument _document = null;
+        protected HtmlAgilityPack.HtmlNode Document
+        {
+            get
+            {
+                if (_document == null)
+                {
+                    string sMoviePageContents = PageFetch(PageAddress);
+                    _document = new HtmlAgilityPack.HtmlDocument();
+                    _document.LoadHtml(sMoviePageContents);
+                }
+                return _document.DocumentNode;
+            }
+        }
+
+        public List<MovieResult> Find(bool showOnlyRelatedFilms = true)
         {
             var lstMovies = new List<MovieResult>();
 
-            title = Utils.FixTitleForSearching(title); // fix wrnog symbols
-            var pattern = new Regex(@"\s+");
-            title = string.Join("+", pattern.Split(title));
-            if (!string.IsNullOrEmpty(year)) title += "+" + year;
-            string contents = PageFetch("http://www.kinopoisk.ru/level/7/type/film/list/1/find/" + System.Uri.EscapeUriString(title));
-
-            var html = new HtmlAgilityPack.HtmlDocument();
-            html.LoadHtml(contents);
-
-            HtmlAgilityPack.HtmlNode document = html.DocumentNode;
-            IEnumerable<HtmlAgilityPack.HtmlNode> films = document.QuerySelectorAll("body div.search_results div.info");
-
-            var idPattern = new Regex(@"\/film\/(\d+)\/");
-            var titlePattern = new Regex("[А-Яа-я]+");
+            IEnumerable<HtmlAgilityPack.HtmlNode> films = showOnlyRelatedFilms
+                                                        ? Document.QuerySelectorAll("body div.search_results div.most_wanted div.info")
+                                                        : Document.QuerySelectorAll("body div.search_results div.info");
 
             foreach (HtmlAgilityPack.HtmlNode filmHeader in films)
             {
-                // define whether film title is local
-                string confirmMovieTitle = titlePattern.Match(title).Success ? GetLocalFilmTitle(filmHeader) : GetGlobalFilmTitle(filmHeader);
-                string confirmMovieYear = filmHeader.QuerySelector("p.name span.year").InnerText;
-
-                string confirmMovieIDs = null;
-                var matches = idPattern.Match(filmHeader.QuerySelector("a").Attributes["href"].Value);
-                if (matches.Groups.Count == 2)
+                MovieResult film = GetMovieResult(filmHeader);
+                if (film != null && film.Year == FilmYear)
                 {
-                    confirmMovieIDs = matches.Groups[1].Value;
-                }
-
-
-                if (confirmMovieYear == year && !string.IsNullOrEmpty(confirmMovieIDs))
-                {
-                    lstMovies.Add(new MovieResult() { ID = confirmMovieIDs, Title = confirmMovieTitle, Year = confirmMovieYear });
+                    lstMovies.Add(film);
                 }
             }
             return lstMovies;
         }
+
+        protected MovieResult GetMovieResult(HtmlAgilityPack.HtmlNode filmNode)
+        {
+            var idPattern = new Regex(@"\/film\/(\d+)\/");
+            var titlePattern = new Regex("[А-Яа-я]+");
+
+            string confirmMovieTitle = null,
+                   confirmMovieYear = null,
+                   confirmMovieIDs = null,
+                   filmId;
+            
+            try
+            {
+                // define whether film title is local
+                confirmMovieTitle = titlePattern.Match(FilmTitle).Success ? GetLocalFilmTitle(filmNode) : GetGlobalFilmTitle(filmNode);
+                confirmMovieYear = filmNode.QuerySelector("p.name span.year").InnerText;
+                filmId = filmNode.QuerySelector("a").Attributes["href"].Value;
+            }
+            catch (Exception)
+            {
+                filmId = confirmMovieTitle = confirmMovieYear = null;
+            }
+            if (!string.IsNullOrEmpty(filmId) )
+            {
+                var matches = idPattern.Match(filmId);
+                if (matches.Groups.Count == 2)
+                {
+                    confirmMovieIDs = matches.Groups[1].Value;
+                }
+            }
+            if (confirmMovieIDs != null && confirmMovieTitle != null && confirmMovieYear != null)
+                return new MovieResult() {ID = confirmMovieIDs, Title = confirmMovieTitle, Year = confirmMovieYear};
+            return null;
+        }
+
 
         protected string GetLocalFilmTitle(HtmlAgilityPack.HtmlNode info)
         {
